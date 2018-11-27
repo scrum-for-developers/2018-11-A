f89d88bb4f1f51190b489e7b249d22a6493af2be
Marco Berger <marco.berger@codecentric.de>
2018-11-27 15:21:40 +0100

    Code Quality: Fixed Sonar major issues

--- worblehat-web/src/main/java/de/codecentric/psd/Worblehat.java
+++ worblehat-web/src/main/java/de/codecentric/psd/Worblehat.java
@@ -1,5 +1,8 @@
 package de.codecentric.psd;
 
+import de.codecentric.psd.worblehat.web.controller.InsertBookController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,17 +12,22 @@ import java.util.Scanner;
 @SpringBootApplication
 public class Worblehat {
 
+	private static final Logger LOG = LoggerFactory
+			.getLogger(InsertBookController.class);
+
+	private Worblehat(){}
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(Worblehat.class, args);
 
 		// this code is basically to (a) demonstrate how to stop a Spring application and (b)
 		// get rid of the SonarQube warning to close the context properly
-		System.out.println("Enter 'stop' to stop Worblehat.");
-		String line = "";
+		LOG.info("Enter 'stop' to stop Worblehat.");
+		String line;
 		do {
 			line = scan.nextLine();
-		} while (!line.equals("stop"));
+		} while (!"stop".equals(line));
 		applicationContext.close();
 	}
 
